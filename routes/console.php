c1da742bddee0794c33d84bcbac1ecc06b3b2ed7
Abiola yakubu <yakubuabiola2003@gmail.com>
2025-02-23 12:38:18 +0100

    updating the route and scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Jobs\FetchCryptoPricesJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+$interval = (int)config('services.crypto_fetch_interval') ?? 60; // Default: 60 seconds; 
+match ($interval) {
+    1 => Schedule::job(new FetchCryptoPricesJob())->everySecond()->withoutOverlapping(),
+    5 => Schedule::job(new FetchCryptoPricesJob())->everyFiveSeconds()->withoutOverlapping(),
+    10 => Schedule::job(new FetchCryptoPricesJob())->everyTenSeconds()->withoutOverlapping(),
+    30 => Schedule::job(new FetchCryptoPricesJob())->everyThirtySeconds()->withoutOverlapping(),
+    60 => Schedule::job(new FetchCryptoPricesJob())->everyMinute()->withoutOverlapping(),
+    default => Schedule::job(new FetchCryptoPricesJob())->everyFiveSeconds()->withoutOverlapping(), // Default
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\CryptoPriceController;
+use App\Livewire\CryptoPrices;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', CryptoPrices::class);
