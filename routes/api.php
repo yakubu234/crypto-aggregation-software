a98a09a9f3bb85bfa50c06feee45fd9060450aab
Abiola yakubu <yakubuabiola2003@gmail.com>
2025-02-24 07:15:28 +0100

    updating the git actiong

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\CryptoPriceController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
 
 Route::get('/crypto-prices', [CryptoPriceController::class, 'index']);
